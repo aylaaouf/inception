1a5adbeb7716574791bc42d5d5778b002cfefc59
aylaaouf <ayoub.laaoufi@hotmail.com>
2026-08-09 15:57:50 +0100

    update name of ssl certificate

--- srcs/requirements/nginx/tools/setup_nginx.sh
+++ srcs/requirements/nginx/tools/setup_nginx.sh
@@ -3,8 +3,8 @@
 mkdir -p /etc/nginx/ssl
 
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-  -keyout /etc/nginx/ssl/myserver.key \
-  -out /etc/nginx/ssl/myserver.crt \
+  -keyout /etc/nginx/ssl/private.key \
+  -out /etc/nginx/ssl/public.crt \
   -subj "/C=MA/ST=Casablanca/L=Casablanca/O=42/OU=1337/CN=aylaaouf.42.fr"
 
 exec nginx -g "daemon off;"

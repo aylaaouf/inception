73311255e293ee107cf10ae2cbd0d8e17c3d1e9c
aylaaouf <ayoub.laaoufi@hotmail.com>
2026-08-09 17:42:31 +0100

    feat(mariadb): add MariaDB Docker setup and initialization script

---
+++ srcs/requirements/mariadb/tools/setup_mariadb.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [[ -d "/var/lib/mysql/mysql" ]]; then
+    echo "mariadb is already initialized"
+else
+    mariadb-install-db --user=mysql --datadir=/var/lib/mysql
+fi
+
+mysqld_safe --datadir=/var/lib/mysql &
+
+until mariadb-admin ping --silent; do
+    sleep 1
+done
+
+mariadb << EOF
+CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+FLUSH PRIVILEGES;
+EOF
+
+mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
+
+exec mysqld_safe --datadir=/var/lib/mysql

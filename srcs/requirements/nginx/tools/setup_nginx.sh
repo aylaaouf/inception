2335adb676eebd806158c361c2cceb30c89ec3db
aylaaouf <ayoub.laaoufi@hotmail.com>
2026-08-09 15:40:10 +0100

    feat: setup nginx

---
+++ srcs/requirements/nginx/tools/setup_nginx.sh
@@ -0,0 +1,8 @@
+mkdir -p /etc/nginx/ssl
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+  -keyout /etc/nginx/ssl/myserver.key \
+  -out /etc/nginx/ssl/myserver.crt \
+  -subj "/C=MA/ST=Casablanca/L=Casablanca/O=42/OU=1337/CN=aylaaouf.42.fr"
+
+exec nginx -g "daemon off;"

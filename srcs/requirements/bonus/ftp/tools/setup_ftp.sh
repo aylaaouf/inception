fe93ff6e7bf978bdce661fbd071bf21fad7a7d1b
aylaaouf <ayoub.laaoufi@hotmail.com>
2026-08-19 15:03:36 +0100

    fix: avoid creating user if already exist and give him permissions of /home/aylaaouf folder

--- srcs/requirements/bonus/ftp/tools/setup_ftp.sh
+++ srcs/requirements/bonus/ftp/tools/setup_ftp.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-adduser --disabled-password --gecos "" ftpuser
+if ! id ftpuser >/dev/null 2>&1; then
+    useradd --home-dir /home/ftpuser --no-create-home ftpuser
+fi
+
 echo "ftpuser:${FTP_PASSWD}" | chpasswd
+
+chown -R ftpuser:ftpuser /home/ftpuser
 
 exec vsftpd /etc/vsftpd/ftp.conf

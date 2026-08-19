ea6e7fe69397c4ce5500ce3f8ee1cdff67f47c3e
aylaaouf <ayoub.laaoufi@hotmail.com>
2026-08-19 10:49:36 +0100

    fix: initialize MariaDB only on first startup

--- srcs/requirements/mariadb/tools/setup_mariadb.sh
+++ srcs/requirements/mariadb/tools/setup_mariadb.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 
-if [[ -d "/var/lib/mysql/mysql" ]]; then
-    echo "mariadb is already initialized"
-else
+if [[ ! -d "/var/lib/mysql/mysql" ]]; then
+
+    echo "Initializing MariaDB..."
+
     mariadb-install-db --user=mysql --datadir=/var/lib/mysql
-fi
 
-mysqld_safe --datadir=/var/lib/mysql &
+    mysqld_safe --datadir=/var/lib/mysql &
 
-until mariadb-admin ping --silent; do
-    sleep 1
-done
+    until mariadb-admin ping --silent; do
+        sleep 1
+    done
 
-mariadb << EOF
-CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
-CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+    mariadb << EOF
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
 EOF
 
-mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
+    mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
+
+else
+
+    echo "MariaDB already initialized"
+
+fi
 
 exec mysqld_safe --datadir=/var/lib/mysql

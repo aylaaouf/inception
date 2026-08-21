6ea1990f77c978c9b197829db0f58a3b24ed63dd
aylaaouf <ayoub.laaoufi@hotmail.com>
2026-08-21 06:07:01 +0100

    fix: fix portainer and ftp

--- srcs/requirements/bonus/ftp/tools/setup_ftp.sh
+++ srcs/requirements/bonus/ftp/tools/setup_ftp.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+mkdir -p /var/run/vsftpd/empty
+chmod 755 /var/run/vsftpd/empty
+
 if ! id ftpuser >/dev/null 2>&1; then
     useradd --home-dir /home/ftpuser --no-create-home ftpuser
 fi
 
 echo "ftpuser:${FTP_PASSWD}" | chpasswd
 
+mkdir -p /home/ftpuser
 chown -R ftpuser:ftpuser /home/ftpuser
+chmod 755 /home/ftpuser
 
 exec vsftpd /etc/vsftpd/ftp.conf
--- srcs/requirements/bonus/portainer/tools/run_portainer.sh
+++ srcs/requirements/bonus/portainer/tools/run_portainer.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-htpasswd -bnBC 10 "" "$(cat /run/secrets/portainer_password.txt)" \
-    | tr -d ':\n' > /tmp/portainer_password_hash
-
 exec /opt/portainer/portainer \
     --data /data \
     --host unix:///var/run/docker.sock \
-    --admin-password-file /tmp/portainer_password_hash
+    --admin-password-file /run/secrets/portainer_password.txt
